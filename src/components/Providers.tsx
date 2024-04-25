54f86d802e8e465f6f4dc81274a9ed52b042dc58
Aditya <codecommander03@gmail.com>
2024-04-25 22:21:50 +0530

    client query added

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,12 @@
 "use client"
 
-const Providers = () => {
-    
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const client = new QueryClient();
+
+const Providers = ({children}: PropsWithChildren<{}>) => {
+    return <QueryClientProvider client={client}>{children}</QueryClientProvider>
 }
 
 export default Providers;
